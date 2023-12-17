d281cf183813254cce43554b50a5807bb9f867ce
SyedHazarath1234 <syed.hazarath@una-brands.com>
2023-12-18 01:05:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Products from './MobileStore/Products'
+import TopNavbar from './MobileStore/TopNavbar'
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-black">Welcome to Syed's Online Mobile Store !</h1>
+      <TopNavbar />
+      <Products />
+    </>
+  )
+}
+
+export default App
---
+++ src/MobileStore/TopNavbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MobileStore.css'
+
+function TopNavbar() {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default TopNavbar
